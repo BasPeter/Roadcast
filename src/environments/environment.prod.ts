dbe3a7a284f39f995bb681c056af4073ec3b7063
BuildTools <unconfigured@null.spigotmc.org>
2020-04-18 19:43:37 +0200

    fix: remove environment files from history

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'x',
+    authDomain: 'x',
+    databaseURL: 'x',
+    projectId: 'x',
+    storageBucket: 'x',
+    messagingSenderId: 'x',
+    appId: 'x',
+    measurementId: 'x'
+  },
+};
